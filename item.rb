a4cdfe452aef1f40c7e5d8e1104d02ef16fdf7d6
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-04-24 23:26:56 +0600

    Added the item class

--- item.rb
+++ item.rb
@@ -1,1 +1,12 @@
-puts 'Hello'
+class Item
+  attr_accessor :publish_date
+  attr_reader :id
+  attr_writer :genre, :author, :source, :label
+
+  def initialize(publish_date, archive)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archive = archive
+  end
+  
+end
