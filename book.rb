83b74378bd0d8b46b4ff1c06901997e5c34f042a
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-24 15:22:23 -0600

    Extend book class from item and call super with item properties

--- book.rb
+++ book.rb
@@ -1,7 +1,11 @@
-class Book
+require 'securerandom'
+require './item'
+
+class Book < Item
   attr_accessor :publisher, :cover_state
   
-  def initialize(publisher, cover_state)
+  def initialize(publish_date, archived, publisher, cover_state, id = SecureRandom.uuid)
+    super(publish_date, archived, id)
     @publisher = publisher
     @cover_state = cover_state
   end
