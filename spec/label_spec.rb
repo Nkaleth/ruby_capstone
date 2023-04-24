63fad1d42fab6858a335ca3f8d5db4d3391d6121
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-24 16:48:50 -0600

    Create unit tests for the initialize method of the label class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -4,4 +4,16 @@ describe Label do
   before :all do
     @label = Label.new('Label', 'red')
   end
+
+  describe '#new' do
+    it 'Should create an instance of the label class' do
+      expect(@label).to be_an_instance_of Label
+    end
+    it 'Should initialize title as label' do
+      expect(@label.title).to eq 'Label'
+    end
+    it 'Should initialize color as red' do
+      expect(@label.color).to eq 'red'
+    end
+  end
 end
