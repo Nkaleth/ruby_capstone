191bf3023e0d3a17b2d152a72883e5cd4a16987a
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-24 17:16:04 -0600

    Create unit tests for the add item method to check if it is added to the label items array

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
+require 'date'
 
 describe Label do
   before :all do
     @label = Label.new('Label', 'red')
+    @item = Item.new(Date.new(2012, 6, 24), true)
+    @label.add_item(@item)
   end
 
   describe '#new' do
@@ -14,6 +17,12 @@ describe Label do
     end
     it 'Should initialize color as red' do
       expect(@label.color).to eq 'red'
+    end
+  end
+
+  describe '#add_item' do
+    it 'Should add an item to the label items array' do
+      expect(@label.instance_variable_get(:@items).length).to be 1
     end
   end
 end
