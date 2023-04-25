25b503d8b4ef0b6a97d86e5273fa51042bc15f86
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-25 01:24:53 -0600

    Create method to request and validate date from user

--- ask.rb
+++ ask.rb
@@ -1,3 +1,20 @@
 class Ask
-  
+  def date
+    year = 0
+    month = 0
+    day = 0
+    loop do
+      puts 'Date'
+      print 'Year: '
+      year = gets.chomp.to_i
+      print 'Month: '
+      month = gets.chomp.to_i
+      print 'Day: '
+      day = gets.chomp.to_i
+      return if day != 0 && month != 0 && year != 0 && month.between?(1, 12) && day.between?(1, 31)
+
+      puts 'Invalid date!'
+    end
+    Date.new(year, month, day)
+  end
 end
