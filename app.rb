b0f0332a648542697057d8a1b9297fd41034f1c3
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-25 01:29:02 -0600

    Create method to add a new book to the books array

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './ask'
+require './book'
 
 class App
   attr_accessor :books, :labels
@@ -7,5 +8,11 @@ class App
     @books = []
     @labels = []
     @ask = Ask.new
+  end
+
+  def add_book
+    book = Book.new(@ask.date, @ask.boolean('Archived?'), @ask.string('Publisher'), @ask.string('Cover state'))
+    @books.push(book)
+    puts "Book created successfully!\n\n"
   end
 end
