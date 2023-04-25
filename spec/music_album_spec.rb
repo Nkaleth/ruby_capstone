9207629428a0315ab0f21a9d048d80b629f545ba
Nkaleth <nseguralu@gmail.com>
2023-04-25 16:08:07 -0500

    ADD test for method can_be_archived?

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -7,4 +7,16 @@ describe MusicAlbum do
     expect(album.genre).to eq('Rock')
     expect(album.publish_date.year).to eq(2022)
   end
+
+  it 'Should return true because album has more than 10 years' do
+    album2 = MusicAlbum.new('2000-01-15', 'Pop', true)
+    MusicAlbum.send(:public, :can_be_archived?)
+    expect(album2.can_be_archived?).to be true
+  end
+
+  it 'Should return false because album should has less than 10 years' do
+    album2 = MusicAlbum.new('2020-01-15', 'Pop', true)
+    MusicAlbum.send(:public, :can_be_archived?)
+    expect(album2.can_be_archived?).to be false
+  end
 end
