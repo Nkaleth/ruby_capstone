cc5800082c10bfc59e59b5278ca8a893256a1433
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-04-25 22:48:42 +0600

    Added unit tests for all implemeneted methods of game class

--- author.rb
+++ author.rb
@@ -1,10 +1,15 @@
 require 'securerandom'
 class Author
-    attr_accessor :first_name, :last_name
-    def initialize(first_name,last_name,items,items,id = SecureRandom.uuid)
-        @first_name = first_name
-        @last_name = last_name
-        @items = items
-          
-    end
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name, items = [], _id = SecureRandom.uuid)
+    @first_name = first_name
+    @last_name = last_name
+    @items = items
+  end
+
+  def add_item=(item)
+    @items.push(item)
+    item.author << self
+  end
 end
