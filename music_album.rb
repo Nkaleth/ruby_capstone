0f8045cfc6fe0ebe5b4b197519638ce24986e9ef
Nkaleth <nseguralu@gmail.com>
2023-04-25 11:20:10 -0500

    ADD properties for Genre class and MusicAlbum according diagram

--- music_album.rb
+++ music_album.rb
@@ -2,5 +2,18 @@ require 'securerandom'
 require './item'
 
 class MusicAlbum < Item
-  puts 'Hello'
+  attr_accessor :genre, :on_spotify
+
+  def initialize(publish_date, archived, genre, on_spotify)
+    super(publish_date, archived)
+    @genre = genre
+    @on_spotify = on_spotify
+    @id = SecureRandom.uuid
+  end
+
+  private
+
+  def can_be_archived?
+    puts 'yes or no?'
+  end
 end
