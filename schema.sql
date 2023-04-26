e2426cde335d4998592e29f7af92ab335514c22d
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-26 09:32:00 -0600

    Add foreign key to books table that references label id

--- schema.sql
+++ schema.sql
@@ -4,10 +4,12 @@ CREATE DATABASE catalog_of_things;
 \c catalog_of_things
 
 CREATE TABLE books(
-  ID INT GENERATED ALWAYS AS IDENTITY,
+  id INT GENERATED ALWAYS AS IDENTITY,
   publish_date DATE,
   archived BOOLEAN,
   publisher VARCHAR(250),
   cover_state VARCHAR(250),
+  label_id INT,
+  CONSTRAINT fk_labels FOREIGN KEY(label_id) REFERENCES labels(id) ON DELETE CASCADE,
   PRIMARY KEY(id)
 );
