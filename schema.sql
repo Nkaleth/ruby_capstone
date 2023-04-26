b0ec34768edf36c9a073a1616c5c079d1fdcc0bc
jorgeabrahan <jorge24abrahan@gmail.com>
2023-04-26 08:38:13 -0600

    Create database and book class table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,13 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE catalog_of_things;
+
+\c catalog_of_things
+
+CREATE TABLE books(
+  ID INT GENERATED ALWAYS AS IDENTITY,
+  publish_date DATE,
+  archived BOOLEAN,
+  publisher VARCHAR(250),
+  cover_state VARCHAR(250),
+  PRIMARY KEY(id)
+);
