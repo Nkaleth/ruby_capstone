aa4e9d60d7e31fb73f69ed65dacff0499290ba2d
Nkaleth <nseguralu@gmail.com>
2023-04-27 10:45:12 -0500

    ADD tests for all situations of can_be_archived? method

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -19,4 +19,28 @@ describe MusicAlbum do
     MusicAlbum.send(:public, :can_be_archived?)
     expect(album2.can_be_archived?).to be false
   end
+
+  it 'Should can be archived because is on spotify and is older than 10 years' do
+    album3 = MusicAlbum.new(Date.new(2000, 1, 15), true, true)
+    MusicAlbum.send(:public, :can_be_archived?)
+    expect(album3.can_be_archived?).to be true
+  end
+
+  it 'Should can not be archived because is not on spotify and is older than 10 years' do
+    album3 = MusicAlbum.new(Date.new(2000, 1, 15), false, false)
+    MusicAlbum.send(:public, :can_be_archived?)
+    expect(album3.can_be_archived?).to be false
+  end
+
+  it 'Should can not be archived because is on spotify and is older than 10 years' do
+    album3 = MusicAlbum.new(Date.new(2000, 1, 15), false, false)
+    MusicAlbum.send(:public, :can_be_archived?)
+    expect(album3.can_be_archived?).to be false
+  end
+
+  it 'Should can not be archived because is on spotify and is not older than 10 years' do
+    album3 = MusicAlbum.new(Date.new(2022, 1, 15), false, false)
+    MusicAlbum.send(:public, :can_be_archived?)
+    expect(album3.can_be_archived?).to be false
+  end
 end
