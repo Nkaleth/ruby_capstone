d1aa3b4d5e7a75b3527408bb28c719c07d03e704
Nkaleth <nseguralu@gmail.com>
2023-04-27 10:49:52 -0500

    FIX rubocop offenses

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -8,18 +8,6 @@ describe MusicAlbum do
     expect(album.publish_date.year).to eq(2022)
   end
 
-  it 'Should return true because album has more than 10 years' do
-    album2 = MusicAlbum.new(Date.new(2000, 1, 15), true, true)
-    MusicAlbum.send(:public, :can_be_archived?)
-    expect(album2.can_be_archived?).to be true
-  end
-
-  it 'Should return false because album should has less than 10 years' do
-    album2 = MusicAlbum.new(Date.new(2020, 1, 15), true, false)
-    MusicAlbum.send(:public, :can_be_archived?)
-    expect(album2.can_be_archived?).to be false
-  end
-
   it 'Should can be archived because is on spotify and is older than 10 years' do
     album3 = MusicAlbum.new(Date.new(2000, 1, 15), true, true)
     MusicAlbum.send(:public, :can_be_archived?)
